143194ebcbbeed1d714baca0c33fe80e8a98479b
Aleksandrdrdr <Alek7765@rambler.ru>
2024-08-18 21:36:02 +0300

    createComicsList

---
+++ src/hooks/http.hook.js
@@ -0,0 +1,33 @@
+import { useState, useCallback } from "react";
+
+export const useHttp = () => { // собственный хук
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const request = useCallback(async (url, method = 'GET', body = null, headers = {'Content-Type': 'aplication/json'}) => { // создали запрос
+
+        setLoading(true); // пока идет запрос, будет идти загрука в виде спиннера
+
+        try {
+            const response = await fetch(url, {method, body, headers});//ответ от запроса будет помещаться response
+
+            if (!response.ok) { // если св-во будет не ок, тогда будем перемещаться в блок с ошибкой в catch
+                throw new Error(`Could not fetch ${url}, status: ${response.status}`); // выкидыаем ошибку
+            }
+
+            const data = await response.json();// ответ записываем в data преобразуя сразу в формат json
+
+            setLoading(false);// когда ответ загрузится, тогда спиннер убираем
+            return data; // после загрузки наш request вернет данные полученные от сервера
+        } catch(e) {
+            setLoading(false); // загрузка прекратится, т.к. вышла ошибка
+            setError(e.message);// получим текст ошибки
+            throw e; // выкидываем ошибку
+        }
+
+    }, []);
+
+    const clearError = useCallback(() => setError(null), []);
+
+    return {loading, request, error, clearError} // возвращаем объекты 
+}
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,29 +1,23 @@
-class MarvelService {
-    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-    _apiKey ='apikey=01fb73c6eb03fc99d4525f617ab7db7f';
-    _baseOfset = 210;
+import { useHttp } from "../hooks/http.hook";
 
-    getResource = async (url) => {
-        let res = await fetch(url);
+const useMarvelService = () => {
+    const {loading, request, error, clearError} = useHttp(); // вытаскиваем наши объекты из хука с помощ деструктуризации
 
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-        }
+    const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    const _apiKey ='apikey=01fb73c6eb03fc99d4525f617ab7db7f';
+    const _baseOfset = 210;
 
-        return await res.json();
+    const getAllCharacters = async (offset = _baseOfset) => {
+        const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
+        return res.data.results.map(_transformCharacter);
     }
 
-    getAllCharacters = async (offset = this._baseOfset) => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
-        return res.data.results.map(this._transformCharacter);
+    const getCharacter = async (id) => {
+        const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
+        return _transformCharacter(res.data.results[0]);
     }
 
-    getCharacter = async (id) => {
-        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
-        return this._transformCharacter(res.data.results[0]);
-    }
-
-    _transformCharacter = (char) => {
+    const _transformCharacter = (char) => {
         return {
             id: char.id,
             name: char.name,
@@ -34,6 +28,8 @@ class MarvelService {
             comics: char.comics.items
         }
     }
+
+    return {loading, error, clearError, getAllCharacters, getCharacter} // возвращаем сущности, т.е. экспортируем их когда будет вызываться наш хук
 }
 
-export default MarvelService;
+export default useMarvelService;
