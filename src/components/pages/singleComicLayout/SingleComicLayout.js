a0bf38df4d6d7957b324ea60f7391b137905e3f6
Соколов Александр Александрович <Sokolov_aa@office.client.loc>
2024-08-29 16:10:41 +0300

    SEO

--- src/components/pages/singleComicLayout/SingleComicLayout.js
+++ src/components/pages/singleComicLayout/SingleComicLayout.js
@@ -1,14 +1,22 @@
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 import './singleComicLayout.scss';
 
-const SingleComicLayout = ({data}) => {
+const SingleComicLayout = ({ data }) => {
 
-    const {title, description, pageCount, thumbnail, language, price} = data;
+    const { title, description, pageCount, thumbnail, language, price } = data;
 
     return (
-       <div className="single-comic">
-            <img src={thumbnail} alt={title} className="single-comic__img"/>
+        <div className="single-comic">
+            <Helmet>
+                <meta
+                    name="description"
+                    content={`${title} comics book`}
+                />
+                <title>{title}</title>
+            </Helmet>
+            <img src={thumbnail} alt={title} className="single-comic__img" />
             <div className="single-comic__info">
                 <h2 className="single-comic__name">{title}</h2>
                 <p className="single-comic__descr">{description}</p>
@@ -17,7 +25,7 @@ const SingleComicLayout = ({data}) => {
                 <div className="single-comic__price">{price}</div>
             </div>
             <Link to="/comics" className="single-comic__back">Back to all</Link>
-        </div> 
+        </div>
     )
 }
 
